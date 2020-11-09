d1b9141484144527b4d8ee32bd80291c256d49c8
Wilsonmfreire <62375886+Wilsonmfreire@users.noreply.github.com>
2020-11-09 08:18:39 -0300

    Update ADiferençaEntreVarELet.js

--- Variaves/ADiferençaEntreVarELet.js
+++ Variaves/ADiferençaEntreVarELet.js
@@ -1,1 +1,8 @@
+/*Vamos repetir o mesmo passo que antes, mas em vez de var, você deve usar let. Para isso, você terá
+que modificar as seguintes variáveis ​​declaradas com var e alterá-las para let.*/
 
+let nome;
+let sobrenome;
+let idade;
+let telefone;
+let sabeProgramar;
